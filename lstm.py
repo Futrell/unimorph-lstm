5ad3c7848f5adfbb2a946a20170e64612a70bd7e
Futrell <subutai@mailinator.com>
2020-11-19 10:15:02 -0800

    unimorph training + generation works

--- lstm.py
+++ lstm.py
@@ -1,8 +1,14 @@
 import sys
 import itertools
 
+import numpy as np
 import torch
 import random
+
+INF = float('inf')
+
+PADDING = '<!PAD!>'
+EOS = '<!EOS!>'
 
 PADDING_IDX = 0
 EOS_IDX = 1
@@ -12,7 +18,7 @@ flat = itertools.chain.from_iterable
 class LSTM(torch.nn.Module):
     def __init__(self, vocab_size, embedding_dim, num_layers, hidden_size, padding_idx=PADDING_IDX, eos_idx=EOS_IDX):
         super().__init__()
-        
+
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
         self.num_layers = num_layers
@@ -45,29 +51,72 @@ class LSTM(torch.nn.Module):
 
     def get_initial_state(self, batch_size):
         init_a, init_b = self.initial_state
-        return torch.stack([init_a]*batch_size, dim=-2), torch.stack([init_b]*batch_size, dim=-2)
+        return torch.stack([init_a]*batch_size, dim=-2), torch.stack([init_b]*batch_size, dim=-2) # L x B x H
 
-    def encode(self, X, lengths):
+    def encode(self, X):
         # X is of shape B x T 
         batch_size, seq_len = X.shape
-        assert batch_size == len(lengths)
         embedding = self.word_embedding(X)
-        output, hidden = self.lstm(embedding, self.get_initial_state(batch_size))
-        return output, hidden 
+        init_h, init_c = self.get_initial_state(batch_size) # init_h is L x B x H
+        output, (out_h, out_c) = self.lstm(embedding, (init_h, init_c)) # output is B x T x H
+        # Also need to include the initial state itself
+        # And the last state is after consuming EOS, so it doesn't matter
+        # So add the initial state in first, and remove the final state
+        full_output = torch.cat((init_h[-1].unsqueeze(-2), output), dim=-2)[:, 0:-1, :]
+        return full_output
 
     def decode(self, h):
-        return torch.log_softmax(self.decoder(h), -1)
+        logits = self.decoder(h)
+        # Remove probability mass from the pad token
+        logits[:, :, self.padding_idx] = -INF
+        return torch.log_softmax(logits, -1)
 
     def lm_loss(self, Y, Y_hat):
         # Y contains the target token indices. Shape B x T
-        # Y_hat contains distributions. Shape B x T x V
+        # Y_hat contains distributions. Shape B x (T+1) x V
         Y_flat = Y.view(-1) # flatten to B*T
         Y_hat_flat = Y_hat.view(-1, self.vocab_size) # flatten to B*T x V
-        mask = (Y_flat != self.padding_idx).float()
-        num_tokens = torch.sum(mask).item()
-        Y_hat_masked = Y_hat_flat[range(Y_hat_flat.shape[0]), Y_flat] * mask
-        ce_loss = -torch.sum(Y_hat_masked) / num_tokens
+        mask = (Y_flat == self.padding_idx)
+        num_tokens = torch.sum(~mask).item()
+        Y_hat_correct = Y_hat_flat[range(Y_hat_flat.shape[0]), Y_flat]
+        Y_hat_correct[mask] = 0
+        ce_loss = -torch.sum(Y_hat_correct) / num_tokens
         return ce_loss
+
+    def train_lm(self, data, print_every=10, num_epochs=1000, batch_size=None, **kwds):
+        if batch_size is None:
+            batch_size = len(data)    
+        opt = torch.optim.Adam(params=self.parameters(), **kwds)
+        for i in range(num_epochs):
+            opt.zero_grad()
+            batch = random.sample(data, batch_size) # shape B x T
+            padded_batch = pad_sequences(batch)
+            z = self.encode(padded_batch) # shape B x (T+1) x H
+            y_hat = self.decode(z) # shape B x (T+1) x V
+            #y = torch.roll(padded_batch, -1, -1) # shape B x T
+            loss = self.lm_loss(padded_batch, y_hat)
+            loss.backward()
+            opt.step()
+            if i % print_every == 0:
+                print("epoch %d, loss = %s" % (i, str(loss.item())), file=sys.stderr)
+
+    def distro_after(self, sequence):
+        sequence = list(sequence) + [PADDING_IDX]
+        padded = pad_sequences([sequence])
+        encoded = self.encode(padded)
+        predicted = self.decode(encoded)[0,-1,:]
+        return predicted
+
+    def generate(self):
+        so_far = []
+        while True:
+            predicted = self.distro_after(so_far).exp().detach().numpy()
+            sampled = np.random.choice(range(len(predicted)), p=predicted)
+            yield sampled
+            if sampled == self.eos_idx:
+                break
+            else:
+                so_far.append(sampled)
 
 def pad_sequences(xs, padding_idx=PADDING_IDX):
     batch_size = len(xs)
@@ -77,29 +126,10 @@ def pad_sequences(xs, padding_idx=PADDING_IDX):
     for i, length in enumerate(lengths):
         sequence = xs[i]
         padded[i, 0:length] = torch.Tensor(sequence[:length])
-    return padded, lengths
-
-def train_lm(lstm, data, print_every=10, num_epochs=1000, batch_size=None, **kwds):
-    # Inputs should have EOS tokens
-    if batch_size is None:
-        batch_size = len(data)    
-    opt = torch.optim.Adam(params=lstm.parameters(), **kwds)
-    for i in range(num_epochs):
-        opt.zero_grad()
-        batch = random.sample(data, batch_size) # shape B x T
-        padded_batch, lengths = pad_sequences(batch)
-        z, _ = lstm.encode(padded_batch, lengths) # shape B x T x H
-        y_hat = lstm.decode(z) # shape B x T x V
-        # x_t -> h_t -> y_t which predicts x_{t+1}
-        y = torch.roll(padded_batch, -1, -1) # shape B x T
-        loss = lstm.lm_loss(y, y_hat)
-        loss.backward()
-        opt.step()
-        if i % print_every == 0:
-            print("epoch %d, loss = %s" % (i, str(loss.item())))
-    return lstm
+    return padded
 
 def example(**kwds):
+    """ Minimum possible loss is 1/5 ln 5 = 0.3219 """
     data = [
         [2,2,EOS_IDX],
         [3,3,3,EOS_IDX],
@@ -109,14 +139,15 @@ def example(**kwds):
     ]
     vocab_size = len(data) + 2
     lstm = LSTM(vocab_size, vocab_size, 2, 10)
-    return train_lm(lstm, data, **kwds)
+    lstm.train_lm(data, **kwds)
+    return lstm
 
 class Indexer:
     def __init__(self, eos_idx=EOS_IDX, padding_idx=PADDING_IDX):
         self.counter = itertools.count()
-        self.seen = {}
         self.eos_idx = eos_idx
         self.padding_idx = padding_idx
+        self.seen = {EOS: self.eos_idx, PADDING: self.padding_idx}
         
     def index_for(self, x):
         if x in self.seen:
@@ -130,8 +161,8 @@ class Indexer:
         
 def format_sequences(xss):
     indexer = Indexer()
-    for xs in xss:
-        yield list(map(indexer.index_for, xs))
+    result = [list(map(indexer.index_for, xs)) for xs in xss]
+    return result, indexer.seen
 
 def read_unimorph(filename, field=1):
     with open(filename) as infile:
@@ -140,11 +171,15 @@ def read_unimorph(filename, field=1):
                 parts = line.strip().split("\t")
                 yield parts[field].casefold()
 
-def train_unimorph_lm(lang, hidden_size=100, num_layers=2, **kwds):
-    data = list(format_sequences(read_unimorph("/Users/canjo/data/unimorph/%s" % lang)))
-    vocab_size = len(set(flat(data))) + 2
+def train_unimorph_lm(lang, hidden_size=100, num_layers=2, batch_size=5, **kwds):
+    data, vocab = list(format_sequences(read_unimorph("/Users/canjo/data/unimorph/%s" % lang)))
+    print("Loaded data for %s..." % lang, file=sys.stderr)
+    vocab_size = len(vocab)
+    print("Vocab size: %d" % vocab_size, file=sys.stderr)
     lstm = LSTM(vocab_size, vocab_size, num_layers, hidden_size)
-    return train_lm(lstm, data, **kwds)
+    print(lstm, file=sys.stderr)
+    lstm.train_lm(data, batch_size=batch_size, num_epochs=10000, print_every=500, **kwds)
+    return lstm, vocab
 
 if __name__ == '__main__':
     train_unimorph_lm(*sys.argv[1:])
